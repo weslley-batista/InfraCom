79c2b65f20bd2edbb1f442c92579e03fe0daa117
Weslley Batista <55666691+weslley-batista@users.noreply.github.com>
2021-12-14 22:16:10 -0300

    enumerado

--- monitoria 3/UDP_client.py
+++ monitoria 3/UDP_client.py
@@ -4,7 +4,8 @@ import threading
 import time
 
 ipCliente1 = ('localhost', 55555)
-portafonte = 50001
+#portafonte = 50001
+portafonte = 64001
 
 socket_cliente1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket_cliente1.bind(('localhost', portafonte))
@@ -28,25 +29,27 @@ print('Porta destino:   {}\n'.format(destPort))
 
 #p2p
 data, adress = socket_cliente1.recvfrom(128)
+msgCheck = "mensagem recebida com sucesso"
 
 #listener colocar data e nome no print
 def listen():
     while 1:
-        #dataHora = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         msgClient = socket_cliente1.recv(1024)
-        print(' cliente 2: {}\n'.format(msgClient.decode()))
-        
-        
+        print('cliente 2: {}\n'.format(msgClient.decode()))
+        #socket_cliente1.sendto(b'mensagem recebida com sucesso', (ip, sourcePort))
 
 listener = threading.Thread(target=listen)
 listener.start()
 
 #send
 def send():
+    countMsg = 0
     while 1:
+        countMsg = countMsg + 1
         dataHora = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         dataHora = str(dataHora)
-        msgSend = input(dataHora +' > ')
+
+        msgSend = input('{}'.format(countMsg) + ' '+ dataHora +' > ')
         socket_cliente1.sendto(msgSend.encode(), (ip, sourcePort))
 
 enviando = threading.Thread(target=send)
--- monitoria 3/UDP_cliente2.py
+++ monitoria 3/UDP_cliente2.py
@@ -5,7 +5,8 @@ import time
 
 #ipCliente2 = ('147.182.184.215', 55555)
 ipCliente2 = ('localhost', 55555)
-portafonte = 50002
+#portafonte = 50002
+portafonte = 64002
 
 socket_cliente2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket_cliente2.bind(('localhost', portafonte))
@@ -32,20 +33,22 @@ socket_cliente2.sendto(b'0', (ip, sourcePort))
 #listener colocar data e nome no print
 def listen():
     while 1:
-        #dataHora = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         msgClient = socket_cliente2.recv(1024)
         print('cliente 1: {}\n'.format(msgClient.decode()))
-        
+        #socket_cliente2.sendto(b'mensagem recebida com sucesso', (ip,sourcePort))
 
 listener = threading.Thread(target=listen)
 listener.start()
 
 #send
 def send():
+    countMsg = 0
     while 1:
+        countMsg = countMsg + 1
         dataHora = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         dataHora = str(dataHora)
-        msgSend = input(dataHora +' > ')
+
+        msgSend = input('{}'.format(countMsg) + ' ' + dataHora +' > ')
         socket_cliente2.sendto(msgSend.encode(), (ip, sourcePort))
 
 enviando = threading.Thread(target=send)
