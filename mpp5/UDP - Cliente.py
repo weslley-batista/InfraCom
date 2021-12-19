d3d675d45b911f1b904e5de5d3d9046772fe441d
Weslley Batista <55666691+weslley-batista@users.noreply.github.com>
2021-12-19 16:59:43 -0300

    comunicação funcional
    ambos os lados enviam e recebem mensagens

--- mpp5/UDP - Cliente.py
+++ mpp5/UDP - Cliente.py
@@ -1,18 +1,33 @@
 #codigo configurado na maquina de weslley
 import socket
 from typing import ByteString
+import threading
 
-serverAddressPort = ('201.59.169.4', 50005) #colocar ip do servidor e porta, configurados no roteador-servidor
+#serverAddressPort = ('201.59.169.4', 50005) #colocar ip do servidor e porta, configurados no roteador-servidor (julio)
+serverAddressPort = ('45.166.51.58', 1317) #colocar ip do servidor e porta, configurados no roteador-servidor (pedro)
+
 bufferSize = 1024
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) 
-inputClient=""
+def envia():
+    countmsg = 0
+    inputClient=""
+    while (inputClient != 'quit'):
+        while True:
+            countmsg = countmsg+1
 
-while (inputClient != 'quit'):
-    inputClient = input()
-    msgFromClient = bytes(inputClient, 'utf-8') 
+            inputClient = input()
+            msgFromClient = bytes(str(countmsg) +') '+inputClient, 'utf-8') #contador + input
+            UDPClientSocket.sendto(msgFromClient, serverAddressPort)
 
-    UDPClientSocket.sendto(msgFromClient, serverAddressPort)
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+def recebe():
+    UDPClientSocket.bind(('192.168.1.110', 34942)) #porta para receber mensagem do servidor
+    while True:
+        msgServidor = UDPClientSocket.recvfrom(1024)
+        print("Messagem do servidor: {}".format(msgServidor[0].decode('utf-8')))
 
-    print("Message from server: {}".format(msgFromServer[0].decode('utf-8')))
+enviar = threading.Thread(target=envia)
+receber = threading.Thread(target=recebe)
+
+receber.start()
+enviar.start()
