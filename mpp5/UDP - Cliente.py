b02ea4e22c42f200e841b6fd012867f5a7b5e738
Weslley Batista <55666691+weslley-batista@users.noreply.github.com>
2021-12-21 00:09:23 -0300

    final
    testa em maquinas diferentes

--- mpp5/UDP - Cliente.py
+++ mpp5/UDP - Cliente.py
@@ -4,47 +4,53 @@ from typing import ByteString
 import threading
 import time
 
-#serverAddressPort = ('201.59.169.4', 50005) #colocar ip do servidor e porta, configurados no roteador-servidor (julio)
+serverAddressPort = ('201.59.169.4', 50005) #colocar ip do servidor e porta, configurados no roteador-servidor (julio)
 #serverAddressPort = ('45.166.51.58', 1317) #colocar ip do servidor e porta, configurados no roteador-servidor (pedro)
-serverAddressPort = ('localhost', 50000) #colocar ip do servidor e porta, configurados no roteador-servidor (local)
+#serverAddressPort = ('localhost', 50000) #colocar ip do servidor e porta, configurados no roteador-servidor (local)
 
 bufferSize = 1024
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 def envia():
+    global inputClient
+    inputClient = ''
     global countmsg
     countmsg = 0
     inputClient=""
     while (inputClient != 'quit'):
-        while True: 
-            inputClient = input()
-            msgFromClient = bytes(str(countmsg+1) +') '+inputClient, 'utf-8')
-            UDPClientSocket.sendto(msgFromClient, serverAddressPort)
-            countmsg = countmsg+1
-            #---verificação de intervalo---
-            intervalomsg = countmsg % 10
-            #------------------------------
-            if(intervalomsg == 0 and countmsg!=0):      
-                print("intervalo de 10 segundos")
-                time.sleep(10)
-                if(countmsg==30):
-                    break
+        while True:
+            if(countmsg<30):
+                inputClient = input()
+                msgFromClient = bytes(str(countmsg+1) +' '+inputClient, 'utf-8')
+                UDPClientSocket.sendto(msgFromClient, serverAddressPort)
+                countmsg = countmsg+1
+                #---verificação de intervalo---
+                intervalomsg = countmsg % 10
+                #------------------------------
+                if(intervalomsg == 0 and countmsg!=0):
+                    print("intervalo de 10 segundos")
+                    time.sleep(10)#mudar para 10
+                    if(countmsg==30):
+                        break
+                
 def recebe():
-    #UDPClientSocket.bind(('192.168.1.110', 34942)) #porta para receber mensagem do servidor
-    UDPClientSocket.bind(('localhost', 50001)) #porta para receber mensagem do servidor (local)
+    UDPClientSocket.bind(('192.168.1.110', 34942)) #porta para receber mensagem do servidor
+    #UDPClientSocket.bind(('localhost', 50001)) #porta para receber mensagem do servidor (local)
     countAck=0
     while True:
-        msgServidor = UDPClientSocket.recvfrom(1024)
-        print("Messagem do servidor: {}".format(msgServidor[0].decode('utf-8')))
-        msgServidor = msgServidor[0].decode('utf-8')
-        msgAck = msgServidor.find('ack')
-        
-        if(msgAck != -1):
-            countAck = countAck + 1
-        if(countmsg == 30):
-            taxaPerda = (countmsg-countAck)/countmsg
-            print('taxa de perda: ', taxaPerda)
-            UDPClientSocket.close()
+        if(countmsg<30):
+            msgServidor = UDPClientSocket.recvfrom(1024)
+            print("Messagem do servidor: {}".format(msgServidor[0].decode('utf-8')))
+            msgServidor = msgServidor[0].decode('utf-8')
+            msgAck = msgServidor.find('ack')
+            
+            if(msgAck != -1): #-1 ack nao encontrado
+                countAck = countAck + 1
+            if(countmsg == 30):
+                taxaPerda = (countmsg-countAck)/countmsg
+                print('taxa de perda: ', taxaPerda)
+                UDPClientSocket.close()
+            
 
 enviar = threading.Thread(target=envia)
 receber = threading.Thread(target=recebe)
--- mpp5/UDP - servidor.py
+++ mpp5/UDP - servidor.py
@@ -1,16 +1,15 @@
-#julio
 import socket
 import datetime as dt
 
 localPort = 55555 #porta
 portEnvio = 1080
 bufferSize = 1024
-#endPortaEnvio = ('45.6.136.180',1080) #ip e porta do roteador cliente (weslley)
-endPortaEnvio = ('localhost',50001) #ip e porta do roteador cliente (local)
+endPortaEnvio = ('45.6.136.180',1080) #ip e porta do roteador cliente (weslley)
+#endPortaEnvio = ('localhost',50001) #ip e porta do roteador cliente (local)
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-#UDPServerSocket.bind(('192.168.1.7',localPort)) #ip local e porta para receber dados (maquina)
-UDPServerSocket.bind(('localhost',50000)) #ip local e porta para receber dados(local)
+UDPServerSocket.bind(('192.168.1.7',localPort)) #ip local e porta para receber dados (maquina)
+#UDPServerSocket.bind(('localhost',50000)) #ip local e porta para receber dados(local)
 print("O servidor UDP está pronto")
 
 while True:
@@ -19,6 +18,6 @@ while True:
     endPorta = bytesAddressPair[1]
 
     msgdecode = msg.decode('UTF-8')
-    recebido = msgdecode.split(')')    
+    recebido = msgdecode.split(' ')
     msgFormatada = bytes("ack: " + recebido[0], 'utf-8')
-    UDPServerSocket.sendto(msgFormatada, endPortaEnvio)
+    UDPServerSocket.sendto(msgFormatada, endPorta)
